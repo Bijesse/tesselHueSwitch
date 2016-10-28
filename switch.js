e67043ab2abe93897096418a16751747ff5c9ecf
Bijesse <tom@bijesse.com>
2016-10-27 20:15:39 -0400

    light on and bright

--- switch.js
+++ switch.js
@@ -22,4 +22,42 @@ board.on("ready", () => {
     var yellowLed = new five.Led("b4");
     var sensorLed = new five.Led("b5");
 
+    var hueURL = "http://192.168.1.3/api/OGIOGrBPRyn1AutXp3tLkZFIbMETdMaUCIBBkCL7";
+    //request({ url: url, method: 'PUT', json: {foo: "bar", woo: "car"}}, callback)
+
+    //changes light state to on
+    function lightOneOn(){
+    request({
+      url: hueURL + "/lights/1/state",
+      method: 'PUT',
+      json: {"on": true}
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      }
+    }
+    );
+  }
+
+    //turns brightness up light up to 210/256
+    function lightOneBright(){
+    request({
+      url: hueURL + "/lights/1/state",
+      method: 'PUT',
+      json: {"bri": 210}
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      }
+    }
+    );
+  }
+
+
+
+
+
+
 });
